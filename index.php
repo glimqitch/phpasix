449bd7f194b4cd077d92b768b3bf95d3bbea30d1
glimqitch <irismariaqueralt@iesebre.com>
2024-09-25 19:30:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Demo ASIX</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+
+                echo "Hello World!";
+
+            ?>
+        </h1>
+    </body>
+</html>
